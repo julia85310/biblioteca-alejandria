7588963160d0f169e18cd5679cc651a4f117dae2
julia85310 <juliarueda85310@gmail.com>
2025-04-24 02:14:17 +0200

    footer sencillo sin estilos

---
+++ src/app/components/footer.js
@@ -0,0 +1,9 @@
+
+export default function footer(){
+    return <footer className="w-[100vw]">
+        <ul>
+            <li>Tlf: 955975895    8.30h - 22.00h</li>
+            <li>© 2025 Biblioteca Alejandría Todos los derechos reservados</li>
+        </ul>
+    </footer>
+}
