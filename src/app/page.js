ff8a5bef239f908d29cb5ef7e3333385915706ac
julia85310 <juliarueda85310@gmail.com>
2025-04-24 03:05:23 +0200

    html base home

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,39 @@
-import Image from "next/image";
+import Footer from "./components/footer"
+import Header from "./components/header"
+import { useState } from "react"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [eventos, setEventos] = useState([])
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  return <div className="w-[100vw] flex flex-col">
+    <Header ubiHeader="Home"></Header>
+    <main className="flex-1 flex flex-col lg:flex-row">
+      <p className="lg:hidden block">Bienvenido a nuestra querida biblioteca</p>
+      <div id="eventos">
+        {!eventos && <p>La sala de eventos está en pausa. ¡Vuelve pronto a hojear nuevas actividades!</p>}
+        {eventos.map((evento) => {
+          const fecha = new Date(evento.fecha);
+          const fechaFormateada = fecha.toLocaleDateString('es-ES', {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+          });
+          return <div key={evento.id}>
+            <img src={evento.imagen_url} alt="Imagen del evento"></img>
+            <p>{fechaFormateada}</p>
+            <p>evento.descripcion</p>
+            <p>evento.titulo</p>
+          </div>
+        })}
+      </div>
+      <div id="descripcion">
+        <p className="lg:block hidden">Bienvenido a nuestra querida biblioteca</p>
+        <p>Con 70 años de historia, somos un espacio dedicado al conocimiento y al entretenimiento, con un catálogo muy amplio que abarca desde literatura clásica hasta ciencia, arte, historia y mucho más.<br/>
+          Estamos ubicados en Calle de los Álamos, 42, Villa Nieve, donde los libros siguen cobrando vida cada día. Desde esta página puedes explorar nuestro catálogo, reservar ejemplares y consultar si tienes libros pendientes de devolución, para que otras personas también puedan disfrutarlos.<br/>
+          Además, organizamos eventos culturales, talleres y encuentros literarios que mantienen viva la biblioteca y fomentan la pasión por la lectura.<br/>
+          ¡Descubre tu próximo libro con nosotros!</p><br/>
+      </div>
+    </main>
+    <Footer></Footer>
+  </div>
 }
