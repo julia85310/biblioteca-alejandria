872d7d73d1598e14d65ec50e8499df331028c6bd
julia85310 <juliarueda85310@gmail.com>
2025-04-30 02:45:20 +0200

    estilo basico header

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -32,10 +32,10 @@ export default function Header({ubiHeader}){
         }
     }
 
-    return <header>
-        <img src="../logo.png"/> 
+    return <header className={`flex flex-row px-12 pt-10 justify-between pb-6 ${user?.admin ? "bg-[var(--aliceBlue)]" : "bg-[var(--seashell)]"}`}>
+        <img src="../logo.png" className="w-[10vw]"/> 
         <img className="block lg:hidden z-10" src="../iconos/icono-menu-hb.png" onClick={abrirMenu}></img>
-        <nav className={`${menuHidden ? 'hidden' : 'block'} z-20 lg:static lg:block flex flex-col lg:flex-row`}>
+        <nav className={`${menuHidden ? 'hidden' : 'flex'}  z-20 lg:static lg:flex flex-col lg:flex-row justify-end items-end gap-16 px-18`}>
             <p onClick={()=>handleClickNav("Home", ubiHeader)}>Conócenos</p>
             <p onClick={()=>handleClickNav("Catalogo", ubiHeader)}>Catálogo</p>
             <p onClick={()=>handleClickNav("Perfil", ubiHeader)}>{user?.admin ? "Panel de Administración" : "Mi perfil"}</p>
