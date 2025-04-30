57618f003ea816e3097e95363f959de12bb67b8e
julia85310 <juliarueda85310@gmail.com>
2025-04-30 17:08:34 +0200

    estilos footer terminados

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -1,7 +1,7 @@
 
 export default function Footer(){
-    return <footer className="w-[100vw]">
-        <ul>
+    return <footer className="w-screen bg-[var(--linen)] text-[var(--lion)] py-4 lg:py-6 text-center font-admin text-sm">
+        <ul className="flex justify-around items-center gap-1 lg:gap-2 flex-col">
             <li>Tlf: 955975895    8.30h - 22.00h</li>
             <li>© 2025 Biblioteca Alejandría Todos los derechos reservados</li>
         </ul>
