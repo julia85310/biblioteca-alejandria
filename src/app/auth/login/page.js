5d2978b2365a3a02841dbb9ba4f973c518263146
julia85310 <juliarueda85310@gmail.com>
2025-05-02 00:09:18 +0200

    responsivo el login menos en ipad

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -32,45 +32,53 @@ export default function signupPage(){
         
     }
 
-    return <div>
+    return <div className="min-h-[100vh] flex flex-col">
         <Header ubiHeader="Perfil"></Header>
-        <main>
-            <form onSubmit={(e) => loginRequest(e)} className="flex flex-col">
-                <h1>Inicia Sesión</h1>
-                <div>
-                    <img src="/iconos/email-icon.png"></img>
+        <main className="py-4 pb-8 lg:py-4 lg:pb-8 md:py-6 md:px-20 w-[100%] h-[100%] bg-[var(--seashell)] flex-1 flex flex-col justify-center items-center">
+            <form onSubmit={(e) => loginRequest(e)} className="lg:w-[30%] md:w-[60%] w-[75%] px-8 rounded-xl mb-2 py-8 bg-[var(--lion)] flex flex-col gap-4 lg:gap-4 md:gap-8">
+                <h1 className="mb-4 text-white text-center lg:text-xl text-xl md:text-2xl font-bold">Inicia Sesión</h1>
+                <div className="flex bg-[var(--seashell)] rounded pr-2 pl-2 py-1">
+                    <img className="w-4 object-contain mr-2" src="/iconos/email-icon.png" ></img>
                     <input 
                         type="email"
-                        placeholder="correoejemplo@ejemplo.com" 
+                        placeholder="Email" 
+                        className="flex-1 font-admin placeholder-[var(--lion)] md:text-base lg:text-xs text-xs w-auto"
                         onChange={(e) =>
                             setFormData({ ...formData, email: e.target.value })
                         }
                     />
                 </div>
-                <div>
-                    <img src="/iconos/icono-password.png"></img>
+                <div className="flex bg-[var(--seashell)] rounded pr-2 pl-2 py-1">
+                    <img className="w-4 object-contain mr-2" src="/iconos/icono-password.png"></img>
                     <input 
                         type="password"
-                        placeholder="Su contraseña" 
+                        className="flex-1 font-admin placeholder-[var(--lion)] md:text-base lg:text-xs text-xs w-auto" 
+                        placeholder="Contraseña " 
                         onChange={(e) =>
                             setFormData({ ...formData, password: e.target.value })
                         }
                     />
                 </div>
-                {mensaje && <h2>{mensaje}</h2>}
-                <input 
-                    type="checkbox"
-                    onChange={(e) =>
-                        setFormData({ ...formData, recuerdame: e.target.checked })
-                    }
-                    checked
-                /><label>Recuérdame</label>
-                <input 
-                    type="submit"
-                    value="Ok"
-                />
+                {mensaje && <h2 className="lg:text-xs md:text-sm text-sm text-center font-bold">{mensaje}</h2>}
+                <div className="flex">
+                    <input 
+                        type="checkbox"
+                        onChange={(e) =>
+                            setFormData({ ...formData, recuerdame: e.target.checked })
+                        }
+                        className="mr-2 accent-[var(--seashell)] rounded"
+                        checked
+                    /><label className="font-admin text-[var(--seashell)] md:text-base lg:text-xs text-sm">Recuérdame</label>
+                </div>
+                <div className="flex justify-end">
+                    <input 
+                        className="bg-[var(--seashell)] border rounded-4xl px-4 py-1"
+                        type="submit"
+                        value="Ok"
+                    />
+                </div>
             </form>
-            <p onClick={() => router.push("/auth/signup")}>No tengo una cuenta</p>
+            <p className="underline underline-offset-4 lg:text-sm md:text-base text-sm mt-4" onClick={() => router.push("/auth/signup")}>No tengo una cuenta</p>
         </main>
         <Footer></Footer>
     </div>
