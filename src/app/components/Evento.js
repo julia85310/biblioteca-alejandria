a95592dc883dd8ad6b0048701ab3c3fde801b84d
julia85310 <juliarueda85310@gmail.com>
2025-05-01 21:12:17 +0200

    estilos movil

--- src/app/components/Evento.js
+++ src/app/components/Evento.js
@@ -1,14 +1,22 @@
 
 export default function Evento({evento}){
     return <div className="relative w-fit">
-        <div id="fondo" className="z-10 absolute top-0 left-20 right-0 bottom-0 bg-[var(--lion)] "></div>
-        <div className="flex flex-col pr-4">
+        <div id="fondoOrdenador" className="hidden lg:block z-10 absolute top-0 left-20 right-0 bottom-0 bg-[var(--lion)] "></div>
+        
+        <div className="hidden lg:flex lg:flex-col lg:pr-4">
             <p className="relative z-20 text-end text-white mr-2 mt-5 text-sm">{evento.fechaFormateada}</p>
             <div className="flex flex-row">
                 <img className="flex-shrink relative z-20 max-w-[22rem] h-auto object-contain" src={evento.imagen_url} alt="Imagen del evento"></img>
                 <p className="elemento-con-scroll overflow-y-auto max-h-[15rem] min-w-[9rem] w-full whitespace-pre-line flex-1 relative z-20 text-[var(--seashell)] text-[0.55rem] leading-loose ml-4 mt-6 mr-8">{evento.descripcion}</p>
             </div>
             <p className="ml-20 relative z-20 text-center py-8 text-xs">{evento.titulo}</p>
+        </div>
+
+        <div id="movil" className="lg:hidden flex flex-col z-10 mt-4 ml-4 bg-[var(--lion)] ">
+            <img className="-mt-4 -ml-4 z-20 relative max-w-[20rem] h-auto object-contain" src={evento.imagen_url} alt="Imagen del evento"></img>
+            <p className="mr-2 relative z-20 text-end text-white mr-2 mt-5 text-sm ">{evento.fechaFormateada}</p>
+            <p className="relative z-20 text-center px-4 py-4">{evento.titulo}</p>
+            <p className="text-[var(--seashell)] text-center leading-loose relative z-20 elemento-con-scroll overflow-y-auto whitespace-pre-line text-xs px-6 pb-6">{evento.descripcion}</p>
         </div>
     </div>
 }
--- src/app/components/header.js
+++ src/app/components/header.js
@@ -36,8 +36,8 @@ export default function Header({ubiHeader}){
     }
 
     return <header className={`relative flex flex-row items-center lg:items-end px-6 pt-6 lg:px-12 lg:pt-10 justify-between pb-6 ${user?.admin ? "bg-[var(--aliceBlue)]" : "bg-[var(--seashell)]"}`}>
-        <img src="../logo.png" className="w-24 lg:w-32"/> 
-        <img className={`${!menuHidden && 'hidden'} lg:hidden z-10 w-12 h-10`} src="../iconos/icono-menu-hb.png" onClick={abrirMenu}></img>
+        <img src="../logo.png" className="w-20 lg:w-32"/> 
+        <img className={`${!menuHidden && 'hidden'} lg:hidden z-10 w-10 h-8`} src="../iconos/icono-menu-hb.png" onClick={abrirMenu}></img>
         <nav className={`${menuHidden ? 'hidden' : 'flex'} lg:border-0 pr-12 border-[var(--cafeNoir)] border-2 rounded-xl z-50 ${user?.admin ? "bg-[var(--aliceBlue)]" : "bg-[var(--seashell)]"} pl-12 py-8 top-6 right-4 pr-4 text-center lg:text-base text-sm gap-6 lg:relative absolute z-20 lg:static lg:flex flex-col lg:flex-row justify-end items-end lg:gap-18 lg:px-12 lg:pb-4`}>
             <img className={`${menuHidden && 'hidden'} w-4 absolute top-3 right-3`} onClick={() => setMenuHidden(!menuHidden)} src="/iconos/icono-close.png"></img>
             <p className={`w-full underline-animate ${ubiHeader === "Home" ? "active" : ""} ${animating === "Home" ? "animate" : ""}`} onClick={()=>handleClickNav("Home", ubiHeader)}>Conócenos</p>
