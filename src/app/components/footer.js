43cfe2aa4848c303088531e65e2c68a0b6f7ec66
julia85310 <juliarueda85310@gmail.com>
2025-05-01 22:01:46 +0200

    pagina principal terminada

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -1,9 +1,9 @@
 
 export default function Footer(){
-    return <footer className="w-screen bg-[var(--linen)] text-[var(--lion)] py-4 lg:py-6 text-center font-admin text-sm">
-        <ul className="flex justify-around items-center gap-1 lg:gap-2 flex-col">
-            <li>Tlf: 955975895    8.30h - 22.00h</li>
-            <li>© 2025 Biblioteca Alejandría Todos los derechos reservados</li>
+    return <footer className="w-screen bg-[var(--linen)] text-[var(--lion)] py-4 text-center font-admin text-sm">
+        <ul className="flex justify-around items-center gap-1 flex-col">
+            <li className="lg:text-xs">Tlf: 955975895    8.30h - 22.00h</li>
+            <li className="lg:text-xs">© 2025 Biblioteca Alejandría Todos los derechos reservados</li>
         </ul>
     </footer>
 }
--- src/app/page.js
+++ src/app/page.js
@@ -19,9 +19,9 @@ export default function Home() {
     fetchData(); 
   }, []);
 
-  return <div className="w-[100vw] h-[100vh] flex flex-col bg-[var(--seashell)] ">
+  return <div className="lg:h-[100vh] w-[100%] h-[100%] flex flex-col bg-[var(--seashell)] ">
     <Header ubiHeader="Home"></Header>
-    <main className="flex-1 flex flex-col lg:flex-row justify-around">
+    <main className="flex-1 flex flex-col lg:flex-row justify-around px-2">
       <h2 className="lg:hidden block font-bold text-xl ml-6 mt-2">Bienvenido a nuestra <br/> querida biblioteca</h2>
       <div id="eventos">
         {!eventos? 
@@ -37,9 +37,9 @@ export default function Home() {
           </div>
         }
       </div>
-      <div id="descripcion">
-        <h2 className="lg:block hidden">Bienvenido a nuestra querida biblioteca</h2>
-        <p className="text-center px-8 py-6"><b>Con 70 años de historia</b>, somos un espacio dedicado al conocimiento y al entretenimiento, con un catálogo muy amplio que abarca desde literatura clásica hasta ciencia, arte, historia y mucho más.<br/><br/>
+      <div id="descripcion" className="px-10 lg:ml-8">
+        <h2 className="lg:block hidden lg:pl-4 lg:text-xl"><b>Bienvenido a nuestra <br/> querida biblioteca</b></h2>
+        <p className="lg:text-start text-center px-4 py-6 lg:text-xs"><b>Con 70 años de historia</b>, somos un espacio dedicado al conocimiento y al entretenimiento, con un catálogo muy amplio que abarca desde literatura clásica hasta ciencia, arte, historia y mucho más.<br/><br/>
           Estamos ubicados en <b>Calle de los Álamos, 42, Villa Nieve</b>, donde los libros siguen cobrando vida cada día. Desde esta página puedes explorar nuestro catálogo, <b>reservar</b> ejemplares y <b>consultar</b> si tienes libros pendientes de devolución, para que otras personas también puedan disfrutarlos.<br/><br/>
           Además, organizamos <b>eventos</b> culturales, talleres y encuentros literarios que mantienen viva la biblioteca y fomentan la pasión por la lectura.<br/><br/>
           <b>¡Descubre tu próximo libro con nosotros!</b>
