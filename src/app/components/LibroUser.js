c7cf1983f5f48453acb671e3dbbbc6a24bd26560
julia85310 <juliarueda85310@gmail.com>
2025-05-25 13:12:27 +0200

    perfil responsive basico terminado

--- src/app/components/LibroUser.js
+++ src/app/components/LibroUser.js
@@ -1,9 +1,11 @@
 'use client'
 import {useState, useEffect } from "react"
+import { useRouter } from "next/navigation";
 
 export default function LibroUser({texto1, texto2, user_libro, esHistorial}){
     
     const [libro, setLibro] = useState();
+    const router = useRouter();
 
     useEffect(() => {
         async function fetchData() {
@@ -23,21 +25,21 @@ export default function LibroUser({texto1, texto2, user_libro, esHistorial}){
 
     if(!libro) return null;
 
-    return <div className=" flex flex border border-[var(--lion)] border-2 rounded-xl" onClick={() => router.push(`/catalogo/${libro.id}`)}>
+    return <div className=" flex flex border border-[var(--chamoise)] border-2 mr-2 rounded-xl" onClick={() => router.push(`/catalogo/${libro.id}`)}>
         <img className=" w-18 m-2 rounded object-contain" src={libro.imagen_url} alt={"Portada de " + libro.titulo}></img>
-        <div className="m-2 flex flex-col justify-between flex-1">
+        <div className="m-2 flex flex-col justify-between flex-1 flex flex-col">
             <p className="font-bold">{libro.titulo}</p>
-            <p className="text-xs">{libro.editorial}</p>
-            <div>
-                <p className="border-[var(--lion)]">{reservaNoRealizada? "Reservado para": texto1}</p>
-                <p>{user_libro.fecha_adquisicion}</p>
+            <p className="text-xs mb-1">{libro.editorial}</p>
+            <div className="mb-1">
+                <p className="text-[var(--lion)] text-xs">{reservaNoRealizada? "Reservado para": texto1}</p>
+                <p className="text-sm text-right">{new Date(user_libro.fecha_adquisicion).toLocaleDateString('es-ES')}</p>
             </div>
             {reservaNoRealizada?
                 <p>No adquirido</p>
                 :
                 <div>
-                    <p className="border-[var(--lion)]">{texto2}</p>
-                    <p>{user_libro.fecha_devolucion}</p>
+                    <p className="text-[var(--lion)] text-xs">{texto2}</p>
+                    <p className="text-right min-w-30 mr-1">{new Date(user_libro.fecha_devolucion).toLocaleDateString('es-ES')}</p>
                 </div>}    
         </div>
     </div> 
--- src/app/perfil/page.js
+++ src/app/perfil/page.js
@@ -5,6 +5,7 @@ import MyFooter from "../components/MyFooter"
 import { useContext, useState, useEffect } from "react"
 import { useRouter } from "next/navigation";
 import LibroUser from "../components/LibroUser"
+import CalendarSelectEvent from "../components/CalendarSelectEvent";
 
 export default function PerfilPage(){
     const router = useRouter();
@@ -15,6 +16,8 @@ export default function PerfilPage(){
     const [hiddenHistorial, setHiddenHistorial] = useState(true)
     const [hiddenCalendario, setHiddenCalendario] = useState(true)
     const [moreUserData, setMoreUserData] = useState()
+    const [fechaCalendario, setFechaCalendario] = useState("Selecciona un día marcado")
+    const [descripCalendario, setDescripCalendario] = useState("Para ver que tienes pendiente")
 
     useEffect(() => {
         if (window.innerWidth >= 1024) { 
@@ -71,27 +74,27 @@ export default function PerfilPage(){
 
     return <div className="min-h-[100vh] flex flex-col bg-[var(--seashell)] ">
         <MyHeader ubiHeader="Perfil"></MyHeader>
-        <main className="flex-1 flex flex-col lg:flex-row mx-12 gap-8 my-8">
+        <main className="lg:mt-0 lg:mx-4 flex-1 flex flex-col lg:flex-row mx-12 gap-8 my-8 lg:justify-between">
             <div id="infoUser" className="bg-[var(--linen)] p-4 rounded-xl ">
-                <div id="desplegado" className={`${hiddenUser? 'hidden':'flex'} flex-col gap-7`} >
+                <div id="desplegado" className={`${hiddenUser? 'hidden':'flex'} flex-col gap-7 lg:gap-3 `} >
                     <div id="header" className="flex flex-row justify-between mb-2 ">
                         <b>{user?.nombre}</b>
                         <img src="/iconos/icono-flecha.png" onClick={() => setHiddenUser(!hiddenUser)} className={`object-contain w-6 lg:hidden rotate-90`}></img>
                     </div>
-                    <div id="info" className="flex flex-col text-[var(--lion)] text-sm ml-2 gap-6">
+                    <div id="info" className="flex flex-col text-[var(--lion)] text-sm ml-2 gap-6 lg:text-xs lg:gap-4">
                         <div className="flex flex-row gap-2"><b>Email:</b>{user.email}</div>
                         <div className="flex flex-row gap-2"><b>Teléfono:</b>{user.telefono}</div>
                         <div className="flex flex-row gap-2"><b>Fecha de registro:</b>{new Date(user.fecha_registro).toLocaleDateString('es-ES')}</div>
                         <div className="flex flex-row gap-2"><b>Total libros prestados:</b>{moreUserData.totalLibrosPrestados}</div>
                     </div>
                     <hr className="border-t-2 border-[var(--lion)] m-4 " />
-                    <div id="moreInfo" className="text-[var(--chamoise)] text-sm ml-2 flex flex-col gap-3">
+                    <div id="moreInfo" className="text-[var(--chamoise)] text-sm ml-2 flex flex-col gap-3 lg:text-xs">
                         <div className="flex flex-row gap-2"><b>Préstamos máximos simultaneos:</b>{moreUserData.maxLibrosPrestar}</div>
                         <div className="flex flex-row gap-2 mb-4"><b>Reservas máximas simultaneas:</b>{moreUserData.maxLibrosReservar}</div>
                         <div className="flex flex-row gap-2"><b>Libros en posesión:</b>{moreUserData.librosEnPosesion.length}</div>
                         <div className="flex flex-row gap-2"><b>Libros reservados:</b>{moreUserData.librosReservados.length}</div>
                     </div>
-                    <div id="footer" className="flex flex-row justify-between text-[var(--chamoise)] text-sm">
+                    <div id="footer" className="flex flex-row justify-between text-[var(--chamoise)] text-sm lg:text-xs">
                         <div className="flex flex-row items-center gap-3">
                             <div className={`${!moreUserData.penalizado? "bg-[var(--verde)]": "bg-[var(--rojo)]"} w-5 h-5 rounded-xl `}></div>
                             <b>
@@ -100,24 +103,24 @@ export default function PerfilPage(){
                                 : "No penalizado"}
                             </b>
                         </div>
-                        <img src="/iconos/icono-logout.png" onClick={cerrarSesion} className="object-contain w-14"></img>
+                        <img src="/iconos/icono-logout.png" onClick={cerrarSesion} className="object-contain w-14 lg:w-10"></img>
                     </div>
                 </div>
                 <div id="plegado" className={`${!hiddenUser? 'hidden':'flex'} flex-row justify-between`}>
                     <b>{user?.nombre}</b>
-                    <img src="/iconos/icono-flecha.png" onClick={() => setHiddenUser(!hiddenUser)} className="object-contain w-6"></img>
+                    <img src="/iconos/icono-flecha.png" onClick={() => setHiddenUser(!hiddenUser)} className="object-contain w-6 lg:hidden"></img>
                 </div>
             </div>
-            <div id="libros" className="flex flex-col lg:flex-row gap-8">
+            <div id="libros" className="flex flex-col lg:gap-1 gap-8 lg:flex-1 lg:w-full lg:justify-between">
                 <div id="posesion" className="">
-                    <div id="desplegado" className={`${hiddenLPosesion? 'hidden':'flex'} flex-col justify-between p-4 border border-3 border-[var(--seashell)] gap-8`} >
+                    <div id="desplegado" className={`${hiddenLPosesion? 'hidden':'flex'} flex-col justify-between p-4 border border-3 border-[var(--seashell)] gap-8 lg:gap-3`} >
                         <div className="flex flex-row justify-between">
                             <b>Libros en posesion</b>
-                            <img src="/iconos/icono-flecha.png" onClick={() => setHiddenLPosesion(!hiddenLPosesion)} className={`object-contain w-6 rotate-90`}></img>
+                            <img src="/iconos/icono-flecha.png" onClick={() => setHiddenLPosesion(!hiddenLPosesion)} className={`object-contain w-6 rotate-90 lg:hidden`}></img>
                         </div>
                         {moreUserData.librosEnPosesion.length == 0?
-                        <p className="text-center text-lg text-[var(--chamoise)]">No tienes libros prestados. ¡Explora el <b onClick={() => router.push("/catalogo")}>catálogo</b>!</p>
-                        :<div className="flex flex-row overflow-y-auto">
+                        <p className="text-center text-lg text-[var(--chamoise)] lg:text-xs lg:h-full">No tienes libros prestados. ¡Explora el <b onClick={() => router.push("/catalogo")}><u>catálogo</u></b>!</p>
+                        :<div className="flex flex-row overflow-y-auto mx-[-2em] justify-center lg:justify-between elemento-con-scroll">
                             {
                                 moreUserData.librosEnPosesion.map((userLibro) =>
                                 <LibroUser
@@ -133,18 +136,18 @@ export default function PerfilPage(){
                     </div>
                     <div id="plegado" className={`${!hiddenLPosesion? 'hidden':'flex'} flex-row justify-between border border-3 border-[var(--chamoise)] p-4 rounded-xl`}>
                         <b>Libros en posesion</b>
-                        <img src="/iconos/icono-flecha.png" onClick={() => setHiddenLPosesion(!hiddenLPosesion)} className={`object-contain w-6`}></img>
+                        <img src="/iconos/icono-flecha.png" onClick={() => setHiddenLPosesion(!hiddenLPosesion)} className={`object-contain w-6 lg:hidden`}></img>
                     </div>
                 </div>
                 <div id="reservados">
-                    <div id="desplegado" className={`${hiddenLReservados? 'hidden':'flex'} flex-col justify-between p-4 border border-3 border-[var(--seashell)] gap-8`} >
+                    <div id="desplegado" className={`${hiddenLReservados? 'hidden':'flex'} flex-col justify-between p-4 border border-3 border-[var(--seashell)] gap-8 lg:gap-3`} >
                         <div className="flex flex-row justify-between">
                             <b>Libros reservados</b>
-                            <img src="/iconos/icono-flecha.png" onClick={() => setHiddenLReservados(!hiddenLReservados)} className={`object-contain w-6 rotate-90`}></img>
+                            <img src="/iconos/icono-flecha.png" onClick={() => setHiddenLReservados(!hiddenLReservados)} className={`object-contain w-6 rotate-90 lg:hidden`}></img>
                         </div>
                         {moreUserData.librosReservados.length == 0?
-                        <p className="text-center text-lg text-[var(--chamoise)]">¡Reserva un libro para el momento que desees!</p>
-                        :<div className="flex flex-row overflow-y-auto">
+                        <p className="text-center text-lg text-[var(--chamoise)] lg:text-xs lg:h-full">¡Reserva un libro para el momento que desees!</p>
+                        :<div className="flex flex-row overflow-y-auto mx-[-2em] justify-center lg:justify-between elemento-con-scroll">
                             {
                                 moreUserData.librosReservados.map((userLibro) =>
                                 <LibroUser
@@ -160,22 +163,61 @@ export default function PerfilPage(){
                     </div>
                     <div id="plegado" className={`${!hiddenLReservados? 'hidden':'flex'} flex-row justify-between border border-3 border-[var(--chamoise)] p-4 rounded-xl`}>
                         <b>Libros reservados</b>
-                        <img src="/iconos/icono-flecha.png" onClick={() => setHiddenLReservados(!hiddenLReservados)} className="object-contain w-6"></img>
+                        <img src="/iconos/icono-flecha.png" onClick={() => setHiddenLReservados(!hiddenLReservados)} className="object-contain w-6 lg:hidden"></img>
                     </div>
                 </div>
-                <div id="historial" className="border border-3 border-[var(--chamoise)] p-4 rounded-xl">
-                    <div id="desplegado" className={`${hiddenHistorial? 'hidden':'flex'}`} ></div>
-                    <div id="plegado" className={`${!hiddenHistorial? 'hidden':'flex'} flex-row justify-between`}>
+                <div id="historial">
+                    <div id="desplegado" className={`${hiddenHistorial? 'hidden':'flex'} flex-col justify-between p-4 border border-3 border-[var(--seashell)] gap-8 lg:gap-3`} >
+                        <div className="flex flex-row justify-between">
+                            <b>Historial de préstamos</b> 
+                            <img src="/iconos/icono-flecha.png" onClick={() => setHiddenHistorial(!hiddenHistorial)} className={`object-contain w-6 rotate-90 lg:hidden`}></img>
+                        </div>
+                        {moreUserData.historial.length == 0?
+                        <p className="text-center text-lg text-[var(--chamoise)] lg:text-xs lg:h-full">Nada por aquí. Tu historia aún no ha sido escrita.</p>
+                        :<div className="flex flex-row overflow-y-auto mx-[-2em] justify-center lg:justify-between elemento-con-scroll">
+                            {
+                                moreUserData.historial.map((userLibro) =>
+                                <LibroUser
+                                    key={userLibro.id}
+                                    esHistorial={true}
+                                    texto1="Reservado para"
+                                    user_libro={userLibro}
+                                    texto2="Devolución hasta"
+                                ></LibroUser>
+                                )
+                            }
+                        </div>}
+                    </div>
+                    <div id="plegado" className={`${!hiddenHistorial? 'hidden':'flex'} flex-row justify-between border border-3 border-[var(--chamoise)] p-4 rounded-xl`}>
                         <b>Historial de préstamos</b>
-                        <img src="/iconos/icono-flecha.png" onClick={() => setHiddenHistorial(!hiddenHistorial)} className="object-contain w-6"></img>
+                        <img src="/iconos/icono-flecha.png" onClick={() => setHiddenHistorial(!hiddenHistorial)} className="object-contain w-6 lg:hidden"></img>
                     </div>
                 </div>
             </div>
             <div id="calendario" className="bg-[var(--linen)] p-4 rounded-xl">
-                <div id="desplegado" className={`${hiddenCalendario? 'hidden':'flex'}`} ></div>
+                <div id="desplegado" className={`${hiddenCalendario? 'hidden':'flex'} flex-col justify-between gap-4 `} >
+                    <div className="flex justify-between flex-row ">
+                        <b>Calendario de eventos</b> 
+                        <img src="/iconos/icono-flecha.png" onClick={() => setHiddenCalendario(!hiddenCalendario)} className="object-contain w-6 rotate-90 lg:hidden"></img>
+                    </div>
+                    <div id="calendario">
+                        <CalendarSelectEvent
+                            reservas={moreUserData.librosReservados}
+                            prestamos={moreUserData.librosEnPosesion}
+                            historial={moreUserData.historial}
+                            handleClick={(fecha, descrip) => { if(!fecha || !descrip){alert("Ha ocurrido un error. Inténtelo de nuevo más tarde."); return;} setFechaCalendario(fecha); setDescripCalendario(descrip)}}
+                        ></CalendarSelectEvent>
+                    </div>
+                    <b className="text-[var(--chamoise)] text-center text-lg  lg:text-sm">
+                        {fechaCalendario}
+                    </b>
+                    <p className="text-[var(--chamoise)] text-center  lg:text-xs">
+                        {descripCalendario}
+                    </p>
+                </div>
                 <div id="plegado" className={`${!hiddenCalendario? 'hidden':'flex'} flex-row justify-between`}>
                     <b>Calendario</b>
-                    <img src="/iconos/icono-flecha.png" onClick={() => setHiddenCalendario(!hiddenCalendario)} className="object-contain w-6"></img>
+                    <img src="/iconos/icono-flecha.png" onClick={() => setHiddenCalendario(!hiddenCalendario)} className="object-contain w-6 lg:hidden"></img>
                 </div>
             </div>
         </main>
