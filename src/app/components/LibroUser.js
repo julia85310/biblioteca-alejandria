0f7c38a7ce5e9b20994b680ee0f4627050453c89
julia85310 <juliarueda85310@gmail.com>
2025-06-04 22:41:45 +0200

    arreglados estilos de libroUser en pc

--- src/app/components/LibroUser.js
+++ src/app/components/LibroUser.js
@@ -26,14 +26,14 @@ export default function LibroUser({texto1, texto2, user_libro, esHistorial}){
 
     if(!libro) return null;
 
-    return <div className="pr-3 p-1 flex flex border border-[var(--chamoise)] border-2 mr-2 rounded-xl" onClick={() => router.push(`/catalogo/${libro.id}`)}>
-        <img className=" w-24 m-2 rounded object-contain" src={libro.imagen_url} alt={"Portada de " + libro.titulo}></img>
-        <div className="m-2 flex flex-col justify-between flex-1 flex flex-col gap-5">
-            <div className="flex flex-col gap-1">
-                <p className="font-bold">{libro.titulo}</p>
+    return <div className="pr-3 lg:pr-2 p-1 lg:py-0 flex flex border border-[var(--chamoise)] border-2 mr-2 lg:mr-0 rounded-xl" onClick={() => router.push(`/catalogo/${libro.id}`)}>
+        <img className="lg:w-18 w-24 m-2 lg:m-1 rounded object-contain" src={libro.imagen_url} alt={"Portada de " + libro.titulo}></img>
+        <div className="m-2 flex flex-col justify-between flex-1 flex flex-col gap-5 lg:gap-2">
+            <div className="flex flex-col gap-1 lg:gap-0">
+                <p className="font-bold lg:text-sm">{libro.titulo}</p>
                 <p className="text-xs">{libro.editorial}</p>
             </div>
-            <div className="flex flex-col gap-4 ml-4">
+            <div className="flex flex-col gap-4 lg:gap-1 ml-4 lg:ml-2">
                 <div>
                     <p className="text-[var(--lion)] text-xs">{reservaNoRealizada? "Reservado para": texto1}</p>
                     <p className="text-sm text-right">{new Date(user_libro.fecha_adquisicion).toLocaleDateString('es-ES')}</p>
